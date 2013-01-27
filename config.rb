53978d76e4137676d0c4622336d1df038314d1c0
Dan Lucas <dan.lucas@gmail.com>
2013-01-27 17:23:37 -0500

    Configuration

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/b/"
+http_images_path = "/b/images"
+http_fonts_path = "/b/fonts"
+css_dir = "public/b/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
